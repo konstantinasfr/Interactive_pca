880973347a5bfa45b27e46fa734b23b6a40aad8e
kon_sfrintz <konstantina.sfrintzeri@stromasys.com>
2024-06-11 12:34:28 +0300

    colors changed

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Dash(__name__)
 #    'requests_pathname_prefix': '/Interactive_pca/'
 #})
 # Define colors for each class
-colors = {'Antagonist': 'blue', 'Agonist': 'red'}
+colors = {'Antagonist': 'green', 'Agonist': 'orange'}
 
 # Create scatter plot with hover labels
 fig = px.scatter(df_shap_pca, x='PC1', y='PC2', color='ligand type', symbol='classification',
